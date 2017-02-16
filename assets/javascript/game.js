c600aec6a4130786aa09de144269a85a1cf2b03a
Jeff Knowles <JeffKnowles@jeffs-mbp.attlocal.net>
2017-02-15 22:23:23 -0500

    Updated Win/Loss and reset function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -41,23 +41,25 @@ $(document).ready(function() {
       $("#total").text(playerscore);
     }
 
-    // // Win/Lose Alerts
+    // Win/Lose Alerts
 
-    //   function checkWin() {
+      function checkWin() {
+        if (playerscore == targetscore) {
+          wins=wins+1;
+       alert("You Win!");
+       displaywin();
+       reset();
 
-    //   if (playerscore>0) {
-    //     if (playerscore == targetscore) {
-    //       wins++;
-    //    alert("You Win!");
+     }};
 
-    //  }}}});
-
-      //  function checkLoss() {
+       function checkLoss() {
       
-      // if (playerscore>targetscore) {
-      //       losses++;
-      //    alert("You Lose!");
-      //  }
+      if (playerscore>targetscore) {
+            losses=losses+1;
+         alert("You Lose!");
+         displayloss();
+         reset();
+       }}
 
 		//Add value to player total when clicked
 
@@ -65,32 +67,32 @@ $(document).ready(function() {
       $(".bluecrystal").on("click", function() {
         playerscore=playerscore+bluevalue;
         update();
-        // checkLoss();
-        // checkWin();
+        checkLoss();
+        checkWin();
       });
 
       // Green Crystal
       $(".greencrystal").on("click", function() {
       	playerscore=playerscore+greenvalue;
         update();
-        // checkLoss();
-        // checkWin();
+        checkLoss();
+        checkWin();
       });
 
       // Yellow Crystal
       $(".yellowcrystal").on("click", function() {
       	playerscore=playerscore+yellowvalue;
         update();
-        // checkLoss();
-        // checkWin();
+        checkLoss();
+        checkWin();
       });
 
       // Red Crystal
       $(".redcrystal").on("click", function() {
       	playerscore=playerscore+redvalue;
         update();
-        // checkLoss();
-        // checkWin();
+        checkLoss();
+        checkWin();
       });
 
 
@@ -110,7 +112,26 @@ $(document).ready(function() {
 
     displayloss();
 
-});
+    //Reset Function
+
+    function reset () {
+    var targetscore = 0;
+    var playerscore = 0;
+    var bluevalue = 0;
+    var greenvalue = 0;
+    var yellowvalue = 0;
+    var redvalue = 0;
+    var targetscore = Math.floor(Math.random() * ((19-120)+1) + 120);
+    console.log(targetscore);
+    displaytarget();
+    var bluevalue = Math.floor(Math.random() * ((1-12)+1) + 12);
+    var greenvalue = Math.floor(Math.random() * ((1-12)+1) + 12);
+    var yellowvalue = Math.floor(Math.random() * ((1-12)+1) + 12);
+    var redvalue = Math.floor(Math.random() * ((1-12)+1) + 12);
+    }
+
+  });
+
 
 
 
